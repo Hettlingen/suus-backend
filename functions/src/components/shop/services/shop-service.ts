f3bb1dc2f484415b4f9b11ed81e82a6f6e7aa19f
Martin Braun <martinbraun@scoop.ch>
2020-01-20 20:04:48 +0100

    read shop with async await

--- functions/src/components/shop/services/shop-service.ts
+++ functions/src/components/shop/services/shop-service.ts
@@ -1,29 +1,22 @@
 import {Shop} from "../model/shop";
 import {databaseShop} from "../../../index";
-import {mapShopFromDbToShop} from "./shop-mapper";
 
 export class ShopService {
 
     static async getShop(uuidShop: string): Promise<Shop> {
         if (!uuidShop) throw new Error('[myfarmer] Shop-ID is required');
 
-        let query = `SELECT * FROM shop WHERE uuid=${uuidShop}`;
+        const query = `SELECT * FROM shop WHERE uuid=${uuidShop}`;
 
         try {
-            console.log('[myfarmer] START Query');
-            const shopFromDb = databaseShop.query(query);
-            console.log('[myfarmer] END Query');
+            const shopFromDb = await databaseShop.query(query);
 
-            databaseShop.query(query)
-                .then(() => {
-                    return mapShopFromDbToShop(shopFromDb);
-                }).catch(function(error: any){
-                    throw new Error('[myFarmer] Shop doesnt exist on database.')
-            });
-            return shopFromDb;
-        } catch(error){
-            console.log('[myfarmer] ' + error);
-            throw new Error('[myfarmer] Error reading shop from database');
+            console.log('query-result: ' + shopFromDb);
+            console.log('Index 1 of query-result: ' + shopFromDb[0]);
+
+            return shopFromDb[0];
+        } catch(error) {
+            throw new Error('[myfarmer] Error reading shop from database: ' + error);
         }
     }
 }
