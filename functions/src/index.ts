8a43de5f33055df50e677f08d14501bcb30f0c80
Martin Braun <martinbraun@scoop.ch>
2020-02-04 19:42:30 +0100

    auhorization header attribute allowedd

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -18,9 +18,9 @@ export const webApi = functions.https.onRequest(main);
 // Initialize CORS ------------------------------------------------
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.header("Access-Control-Allow-Headers", 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Origin', 'https://myfarmer-3000.web.app');
+    res.header('Access-Control-Allow-Origin', '*');
     next();
 });
 
