cf5f8aca0b518211348682be9e83e6029cc8cc43
Martin Braun <martinbraun@scoop.ch>
2020-07-11 15:24:50 +0200

    make payement at stripe

--- functions/src/components/shop/services/payment-service.ts
+++ functions/src/components/shop/services/payment-service.ts
@@ -8,12 +8,14 @@ export class PaymentService {
     private static stripe = require('stripe')('sk_test_ijLDjyiCzl44BdJvBnNkGfEu00yIsYTPcQ');
 
     static async pay(payment: Payment): Promise<boolean> {
-        console.log('START: CheckoutService.pay');
+        console.log('START: PaymentService.pay');
 
         const token = payment.card.token;
         const amount = payment.invoice.priceTotal.value * 100;
         const currency = payment.invoice.priceTotal.currency;
         let charge;
+
+        console.log('>>Token: ' + token + ' >>Amount: ' + amount);
 
         try {
             charge = await this.stripe.charges.create({
@@ -23,32 +25,30 @@ export class PaymentService {
                 capture: false,
             });
         } catch(error){
-            throw new Error('[myfarmer] ShopService.getShopItem - Error reading Shopitem');
+            console.log(error);
+            throw new Error('[myfarmer] PaymentService.pay - Error charging credit-card');
         }
 
         try {
-            await this.makePayment(charge);
+            return await this.makePayment(charge);
         } catch(error) {
-            this.refundPayment(charge);
-            return Promise.resolve(false);
+            return await this.refundPayment(charge);
         }
-
-        return Promise.resolve(true);
     }
 
-    static makePayment(charge: any) {
+    static makePayment(charge: any): Promise<boolean> {
         try {
-            this.stripe.charges.capture(charge.id)
+            return this.stripe.charges.capture(charge.id)
         } catch(error) {
             throw error;
         }
     }
 
-    static refundPayment(charge: any) {
+    static refundPayment(charge: any): Promise<boolean> {
         try {
-            this.stripe.refunds.create({charge: charge.id});
+            return this.stripe.refunds.create({charge: charge.id});
         } catch(error) {
-
+            throw error;
         }
     }
 }
