8d1dcc0b40ea3898620b1a173f8ec815c69739ae
Hettlingen <martinbraun@scoop.ch>
2020-09-05 12:28:58 +0200

    post fixes

--- functions/src/components/community/blog/blog-routes.ts
+++ functions/src/components/community/blog/blog-routes.ts
@@ -5,6 +5,8 @@ import {Post} from "./model/post";
 
 export class BlogRoutes {
     public static routes(app: any): void {
+
+        // Get a Blog
         app.route('/blogs/:uuidBlog').get(async (request: Request, response: Response) => {
             BlogService.getBlog(request.params.uuidBlog)
                 .then(function(blog: Blog) {
@@ -14,6 +16,7 @@ export class BlogRoutes {
             });
         })
 
+        // Get a Post
         app.route('/posts/:uuidPost').get(async (request: Request, response: Response) => {
             BlogService.getPost(request.params.uuidPost)
                 .then(function(post: Post) {
@@ -22,5 +25,35 @@ export class BlogRoutes {
                 response.status(404).send("Post wasn't found: " + error)
             });
         })
+
+        // Create a Post
+        app.route('/posts').post(async (request: Request, response: Response) => {
+            BlogService.createPost(request.body)
+                .then(function (post: Post) {
+                    response.status(200).send(post);
+                }).catch(function (error: any) {
+                response.status(404).send("Post wasn't created successful: " + error);
+            });
+        });
+
+        // Update a Post
+        app.route('/posts').put(async (request: Request, response: Response) => {
+            BlogService.updatePost(request.body)
+                .then(function (post: Post) {
+                    response.status(200).send(post);
+                }).catch(function (error: any) {
+                response.status(404).send("Post wasn't updated successful: " + error);
+            });
+        });
+
+        // Delete a Post
+        app.route('/posts/:uuidPost').delete(async (request: Request, response: Response) => {
+            BlogService.deletePost(request.params.uuidPost)
+                .then(function (successful: boolean) {
+                    response.status(200).send(successful);
+                }).catch(function (error: any) {
+                response.status(404).send("Post wasn't deleted successful: " + error);
+            });
+        });
     }
 }
