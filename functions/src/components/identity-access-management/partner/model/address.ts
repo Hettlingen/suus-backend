45776d4311bb58f8a771df3571e70a52ecc1170b
Hettlingen <martinbraun@scoop.ch>
2021-05-19 12:58:40 +0200

    configure jest test framework

--- functions/src/components/identity-access-management/partner/model/address.ts
+++ functions/src/components/identity-access-management/partner/model/address.ts
@@ -5,4 +5,6 @@ export class Address {
     public city!: string;
     public postalCode!: string;
     public countryCode!: string;
+    public latitude!: number; // Breitengrad (horizontale Linien, nördlich <=> südlich)
+    public longitude!: number; // Längengrad (senkrechte Linien, östlich <=> westlich)
 }
---
+++ functions/src/components/workplace/services/location-service.ts
@@ -0,0 +1,45 @@
+import {Address} from "../../identity-access-management/partner/model/address";
+import {Client} from "@googlemaps/google-maps-services-js";
+
+export class LocationService {
+
+    public async getAddressesWithinRadius(addressOfMapCenter: Address, addresses: Address[], radiusInKm: number): Promise<Address[]> {
+        console.log('START: LocationService.getTermsOfUse');
+        const center = new google.maps.LatLng(addressOfMapCenter.latitude, addressOfMapCenter.longitude);
+
+        const filteredAddressesWithinRadius = addresses.filter(address => {
+            const markerLoc = new google.maps.LatLng(address.latitude, address.longitude);
+            const distanceInKm = google.maps.geometry.spherical.computeDistanceBetween(markerLoc, center) / 1000;
+
+            if (distanceInKm < radiusInKm) {
+                return address;
+            }
+            return null;
+        });
+
+        return filteredAddressesWithinRadius;
+    }
+
+    public async test(addressOfMapCenter: Address, addresses: Address[], radiusInKm: number): Promise<Address[]> {
+        console.log('START: LocationService.getTermsOfUse');
+
+        const googleMapsService = new Client({});
+
+        googleMapsService
+            .elevation({
+                params: {
+                    locations: [{ lat: 45, lng: -110 }],
+                    key: "asdf",
+                },
+                timeout: 1000, // milliseconds
+            })
+            .then((r) => {
+                console.log(r.data.results[0].elevation);
+            })
+            .catch((e) => {
+                console.log(e.response.data.error_message);
+            });
+
+        return [];
+    }
+}
